c3ab7237c5121007247489d2b95cd55c01ee1ba5
moralejo20 <79731327+moralejo20@users.noreply.github.com>
2022-08-25 22:20:16 +0000

    Delete Proyecto Municipalidad directory

--- Proyecto Municipalidad/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Municipalidad.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
